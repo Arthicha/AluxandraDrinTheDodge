aaafbc9936e742949d8fefb95a9e7e0a62cbdad4
zefirosfenix <zefirosfenix@gmail.com>
2018-04-06 22:44:57 +0700

    commiting test

---
+++ module/MATLAPUTOPPU.py
@@ -0,0 +1,23 @@
+__author__ = 'Penpat Ritprapa'
+__version__ = 1.0
+__description__ = 'Store call matlab and call simulink function'
+
+# you need to install pymatbridge with pip first
+
+
+'''*************************************************
+*                                                  *
+*                 import module                    *
+*                                                  *
+*************************************************'''
+
+import matlab.engine
+
+
+'''*************************************************
+*                                                  *
+*                 random function                  *
+*                                                  *
+*************************************************'''
+
+eng = matlab.engine.start_matlab()
