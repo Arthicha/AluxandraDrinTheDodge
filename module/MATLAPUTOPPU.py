1d0f53198a31b1cfd33d65d59aef110092fda782
zefirosfenix <zefirosfenix@gmail.com>
2018-04-09 00:39:50 +0700

    callmatlabfunction 90% (testing bugs)

--- module/MATLAPUTOPPU.py
+++ module/MATLAPUTOPPU.py
@@ -2,7 +2,9 @@ __author__ = 'Penpat Ritprapa'
 __version__ = 1.0
 __description__ = 'Store call matlab and call simulink function'
 
-# you need to install pymatbridge with pip first
+# you need to install matlab.engine with command line from this link
+# >> https://www.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html <<
+
 
 
 '''*************************************************
@@ -12,14 +14,26 @@ __description__ = 'Store call matlab and call simulink function'
 *************************************************'''
 
 import matlab.engine
-#dummy comment a
 
-#
 '''*************************************************
 *                                                  *
 *                 random function                  *
 *                                                  *
 *************************************************'''
 
+
+y = {'arg1': 3, 'arg2': 5}
 eng = matlab.engine.start_matlab()
-#push bywan
+eng.sim("vdp")
+
+def callMatFunc(funcname,argumentDict,outputs):
+    eng = matlab.engine.start_matlab()
+    a = getattr(eng,str(funcname))(argumentDict,nargout=outputs)
+    return a
+
+# callMatFunc("yourfunc",y,1)
+
+def callSimulink(simulinkName):
+    eng = matlab.engine.start_matlab()
+    # eng.sim(str(simulinkName))
+    eng.sim("vdp")
